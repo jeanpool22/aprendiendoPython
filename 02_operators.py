534f7d5627e30b43327ac10eb0fe55c1a9ac5e04
Jean Pool Zambrano <jean_pool.98@hotmail.com>
2024-07-03 21:25:27 -0500

    Operadores

---
+++ 02_operators.py
@@ -0,0 +1,30 @@
+# Operadores
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+
+print("Hola " + "Python")
+print("Hola " + str(5))
+print("Hola " * 5)
+
+# Operadores comparativos
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("-" * 10)
+
+# Se comprueba una ordenación alfabética por ASCII
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
