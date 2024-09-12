c9a253d5ba871167786545844445c67f419fc24f
Jean Pool Zambrano <jean_pool.98@hotmail.com>
2024-09-11 21:46:34 -0500

    Cadenas de texto

---
+++ 03_cadenas_de_texto.py
@@ -0,0 +1,10 @@
+name = 'Jean'
+last_name = 'Zambrano Hernandez'
+
+print(name * 5)
+print(name + ' ' + last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower())
+print(name.upper())
+print(last_name.strip())
