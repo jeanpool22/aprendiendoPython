d4bf3f647c6e44ecc103b8159ff644167cee0e90
Jean Pool Zambrano <jean_pool.98@hotmail.com>
2024-09-11 22:55:33 -0500

    Operaciones matemáticas

---
+++ 06_operaciones_matematicas.py
@@ -0,0 +1,20 @@
+# Operadores numéricos
+a = 10
+b = 3
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+print("Modulo:", a % b)
+print("Parte entera de la división:", a // b)
+print("Potenciación:", a ** b)
+
+operation_1 = 2 + 3 * 4
+operation_2 = (2 + 3) * 4
+operation_3 = (2 + 3) * (4 ** 2) / 8 - 1
+
+print(operation_1)
+print(operation_2)
+print(operation_3)
