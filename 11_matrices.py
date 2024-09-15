681911a79c655a97ffb4ab5f8c0e11f2e7de4e77
Jean Pool Zambrano <jean_pool.98@hotmail.com>
2024-09-14 22:06:13 -0500

    Matrices

---
+++ 11_matrices.py
@@ -0,0 +1,15 @@
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+print(matrix)
+print(matrix[0])
+
+numero = 1
+numbers = (numero, 2, 3, 4, 5)
+
+print(numbers)
+print(type(numbers))
+print(numbers[0])
+numero = 0
+print(numbers)
